8a5af9e5c3ec07d285b07c3aa05b5e8cb355bf93
Nicholas <nkrekor13@gmail.com>
2021-08-19 09:41:28 -0500

    update sender

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.post('/', cpUpload, function(req, res, next) {
 
         // send mail with defined transport object
         let info = await transporter.sendMail({
-            from: '"Nodemailer" <process.env.EMAIL_USER>', // sender address
+            from: `"Nodemailer" <${req.body.email}>`, // sender address
             to: "<info@cross-trans>", // list of receivers
             subject: "Website Form", // Subject line
             html: htmlContent,
