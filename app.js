d41daa1f668d758407a51e1da8b57c1750a7ba1c
Nicholas <nkrekor13@gmail.com>
2021-08-19 22:53:47 -0500

    nodemailer changes

--- app.js
+++ app.js
@@ -95,8 +95,8 @@ app.post('/', cpUpload, function(req, res, next) {
 
         // send mail with defined transport object
         let info = await transporter.sendMail({
-            from: '"nodemailer" <process.env.SEND>', // sender address
-            to: "info@cross-trans.com", // list of receivers
+            from: "nodemailer", // sender address
+            to: "nkgwebdev@gmail.com", // list of receivers
             subject: "Website Form", // Subject line
             html: htmlContent,
         });
